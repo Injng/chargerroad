28f7a68ae40ace52b2ca17b368a710f99c85dc33
Injng <jlinjiang@gmail.com>
2023-08-14 06:34:54 -0700

    Add components for course list

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
 use yew::prelude::*;
-use stylist::yew::styled_component;
+use stylist::{yew::styled_component, Style};
 
 mod components;
 
 use components::road::Road;
+use components::courses::Courses;
+
+const STYLE_FILE: &str = include_str!("styles/style.css");
+
 
 #[styled_component]
 pub fn App() -> Html {
+    let gen_style = Style::new(STYLE_FILE).unwrap();
     html! {
-        <div>
-            <Road />
+        <div class={gen_style}>
+            <div class="main">
+                <div class="road">   
+                    <Road />
+                </div>
+                <div class="courses">
+                    <Courses />
+                </div>
+            </div>
         </div>
     }
 }
